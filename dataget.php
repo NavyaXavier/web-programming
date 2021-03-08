b2536743b0c6ea6af1c50599a3baefe664cd09d7
NavyaXavier <74652297+NavyaXavier@users.noreply.github.com>
2021-03-08 13:51:45 +0530

    Add files via upload

---
+++ dataget.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<title>get data from another page</title>
+</head>
+<body>
+<?php
+echo "Name: ".$_POST["txtname"]."</br>";
+echo "rollno: ".$_POST["txtr_no"]."</br>";
+echo "address: ".$_POST["add"]."</br>";
+echo "contact no: ".$_POST["txtc_no"]."</br>";
+echo "email: ".$_POST["txteid"]."</br>";
+?>
+</body>
+</html>
